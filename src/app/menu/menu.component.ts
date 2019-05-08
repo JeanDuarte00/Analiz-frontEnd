e4136ee4673c7cec2c60e3f73aa0e0384e98d8c1
Jean Luccas Duarte Brito <luccasduarte320@gmail.com>
2019-05-08 03:29:01 -0300

    hide menu when clicked to see menu bar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,12 +9,15 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
+  hideMenu = true;
+
   ngOnInit() {
   }
 
   openNav() {
     document.getElementById('mySidenav').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
+    this.hideMenu = false;
     // document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
   }
 
@@ -22,6 +25,7 @@ export class MenuComponent implements OnInit {
     document.getElementById('mySidenav').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
     document.body.style.backgroundColor = 'white';
+    this.hideMenu = true;
   }
 
 }
